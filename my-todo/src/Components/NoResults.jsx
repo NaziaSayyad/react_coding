b10089869d465805750fa85cc99d2595a7e1ce93
Nazia Sayyad <sayyadnazia257@gmai.com>
2023-03-17 18:21:39 +0530

    some-updates

---
+++ my-todo/src/Components/NoResults.jsx
@@ -0,0 +1,12 @@
+import { Box, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+function NoResults() {
+  return (
+    <Box>
+        <Heading> No Results Found!!</Heading>
+    </Box>
+  )
+}
+
+export default NoResults
--- my-todo/src/redux/todos/todos.action.js
+++ my-todo/src/redux/todos/todos.action.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { todo_error, todo_loaading, todo_sucess } from "./todos.action.types";
 
-export const Acess_Todos  = async(dispatch) => {
+export const Acess_Todos  = (query) => async(dispatch) => {
     dispatch({type: todo_loaading});
     try{
-          let res =  await axios.get("https://jsonplaceholder.typicode.com/todos")
-            dispatch({type:todo_sucess ,payload:res.data});
+        
+        let res =  await axios.get(`https://jsonplaceholder.typicode.com/todos?q=${query}`)
+            dispatch({type:todo_sucess ,payload:res});
                 return res;
         }
         catch(e) {
